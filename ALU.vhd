a0a4dae3849933b24921e07c279bbcdf6216c122
Abdullah Abujabal <abujabaa@uwindsor.ca>
2017-11-22 15:02:50 -0500

    added ports

--- ALU.vhd
+++ ALU.vhd
@@ -4,11 +4,8 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity ALU is
-	port(
-	
-	
-	
-	
-	);
+	port(Operand1, Operand2: in std_logic_vector(7 downto 0);
+			segment1, segment2: out std_logic_vector(6 downto 0);
+			LED: out std_logic_vector(7 downto 0));
 
 end ALU;
